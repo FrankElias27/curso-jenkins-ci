c6d1c08c71e03e830a0a8f3732a8f0f8fc2edd3d
FRANK <frankmascco@gmail.com>
2025-05-21 19:14:12 -0500

    aplicando cambios

--- src/main/java/com/first/api/controller/HelloController.java
+++ src/main/java/com/first/api/controller/HelloController.java
@@ -52,5 +52,10 @@ public class HelloController {
     public String saludar8(){
         return "Hola Mundo con REST en Spring Boot";
     }
+
+    @GetMapping("/hello9")
+    public String saludar9(){
+        return "Hola Mundo con REST en Spring Boot";
+    }
 }
 
